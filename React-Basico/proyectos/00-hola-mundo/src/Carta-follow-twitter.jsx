e373f5e0f5fee153675080ed88bd2b16382b2027
Bryan406 <rquisper406@gmail.com>
2025-03-19 07:37:26 -0500

    Carta de follow de twitter mitad trabajo

---
+++ React-Basico/proyectos/00-hola-mundo/src/Carta-follow-twitter.jsx
@@ -0,0 +1,20 @@
+export function CartaTwitter({ userName, name, isFollowing }) {
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          alt="avatar de bryan406"
+          src={"https://unavatar.io/${userName}"}
+        />
+        <div className="tw-followCard-info">
+          <strong>{name}</strong>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
+        </div>
+      </header>
+      <aside>
+        <button className="tw-followCard-button">Seguir</button>
+      </aside>
+    </article>
+  );
+}
